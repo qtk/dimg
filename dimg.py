9b689d11d14896a607997b523aa87b0028dc0938
qtk <100johannes100@gmail.com>
2016-05-28 16:50:13 +0200

    Enabled logging

--- dimg.py
+++ dimg.py
@@ -1,25 +1,29 @@
 import bottle
+import logging
+logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 
 dimg = application = bottle.Bottle()
 
 
 @dimg.route("/")
 def show_index():
-    '''
+    """
     :return The front "index" page
-    '''
-    return bottle.static_file(filename="index.html", root="/var/www/dimg/static")
+    """
+    return bottle.static_file(filename="index.html", root="/var/www/dimg")
 
 
 @dimg.post("/validate")
 def validate():
-    '''
+    """
     This page validates the uploaded file
-    :return: A page with the return status
-    '''
+    :return: A page with uploaded file
+    """
     image = bottle.request.files.get("img")
     filename = image.filename
+    logging.info("Uploaded image filename: {}".format(filename))
     image.save(destination="img", overwrite=True)
+
     bottle.redirect("/static/img/" + filename)
 
 if __name__ == "__main__":
