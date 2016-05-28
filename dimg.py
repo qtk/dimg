6a49cc1ed7cc22e4e3d995cc2462c23a02600498
qtk <100johannes100@gmail.com>
2016-05-28 14:57:51 +0200

    Completed rudimentary file upload

--- dimg.py
+++ dimg.py
@@ -6,9 +6,21 @@ dimg = application = bottle.Bottle()
 @dimg.route("/")
 def show_index():
     '''
-    The front "index" page
+    :return The front "index" page
     '''
     return bottle.static_file(filename="index.html", root="./static")
+
+
+@dimg.post("/validate")
+def validate():
+    '''
+    This page validates the uploaded file
+    :return: A page with the return status
+    '''
+    image = bottle.request.files.get("img")
+    filename = image.filename
+    image.save(destination="img", overwrite=True)
+    return bottle.static_file(filename=filename, root="./img")
 
 if __name__ == "__main__":
     bottle.run(app=dimg, host="localhost", port=8080)
